7d58e257ce76bbff349c61951e5a2b7edbf054e0
Zakk Hoyt <vaporwarewolf@gmail.com>
2013-10-23 20:10:11 -0700

    Initial Commit

---
+++ BLETennis/VWWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VWWAppDelegate.h
+//  BLETennis
+//
+//  Created by Zakk Hoyt on 10/23/13.
+//  Copyright (c) 2013 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BLETennis/VWWViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWWViewController.h
+//  BLETennis
+//
+//  Created by Zakk Hoyt on 10/23/13.
+//  Copyright (c) 2013 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWViewController : UIViewController
+
+@end
